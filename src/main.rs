51fb4370e13d32d25b75f75ff2be1ae0dccda848
Bhavya2662 <bhavyaas2662@hotmail.com>
2024-03-05 17:59:27 +0530

    added mta

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,160 @@
+// use curv::elliptic::curves::{secp256_k1::Secp256k1, Point, Scalar};
+
+// use curv::cryptographic_primitives::hashing::hash_sha256;
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(unused_parens)]
+#![allow(dead_code)]
+
+use paillier::*;
+use curv::BigInt;
+use curv::arithmetic::traits::Converter;
+use curv::arithmetic::Zero;
+use curv::arithmetic::Samplable;
 use curv::elliptic::curves::{secp256_k1::Secp256k1, Point, Scalar};
-use curv::arithmetic::traits::Group;
-use curv::cryptographic_primitives::hashing::hash_sha256;
 
-// Input: P1 = (x1,y1) E EC(Fp) from P, P2 = (x2,y2) Є EC(Fp) from V.
-// Output: P and V output s1 and s2 such that s1+s2 =x where (x,y) = P1+P2 in EC.
-pub fn ectf_protocol(p1: &Point<Secp256k1>, p2: &Point<Secp256k1>) -> (Scalar<Secp256k1>, Scalar<Secp256k1>) {
-    // P (and V) sample p; <- Zp for i ∈ {1,2} respectively.
-    let (p1, p2) = (Scalar::random(), Scalar::random());
+fn mta(a: &Scalar<Secp256k1>, b: &Scalar<Secp256k1>) -> (Scalar<Secp256k1>, Scalar<Secp256k1>){
+    let (ek, dk) = Paillier::keypair().keys();
 
-    // P and V run a1, α2 := MtA( (−x1, P1),(p2,x2)).
-    let (a1, a2) = mta(&(-p1.x, p1), &(p2, p2.x));
+    // Alice's input
+    let alice_input= a;
+    // Bob's input
+    let bob_input= b;
 
-    // P computes δ1 =-x1* p1+α1 and V computes δ2 =x2 * p2+ α2.
-    let (d1, d2) = (-p1.x * p1 + a1, p2.x * p2 + a2);
+    // Alice computes cA = EncryptA(a)
+    let c_alice = Paillier::encrypt(&ek, RawPlaintext::from(&alice_input.to_bigint()));
 
-    // P (and V) reveal δ1 (and δ2) to each other and compute δ = δ1 + δ2 .
-    let d = d1 + d2;
+    // Bob selects Beta Tag <– Z(N) -> where n is ek.n 
+    let beta_tag: BigInt = BigInt::sample_below(&ek.n);
 
-    // P (and V) compute ηi = pi* δ-1 for i = {1,2} respectively.
-    let (n1, n2) = (p1 * d.invert(), p2 * d.invert());
+    // Compute Encrypt(BetaTag) using key of A
+    let enc_betatag = Paillier::encrypt(&ek, RawPlaintext::from(&beta_tag));
 
-    //P and V run B1, B2 :=MtA( (-y1, N1), (N2,y2 )).
-    let (b1, b2) = mta(&(-p1.y, n1), &(n2, p2.y));
+    // Compute cB = b * cA + EncryptA(BetaTag) = EncryptA(ab+Tag)
+    // Compute b * CA
+    let b_mul_c_alice = Paillier::mul(&ek, RawPlaintext::from(&bob_input.to_bigint()), c_alice.clone());
 
-    // P computes λ1 =-y1 * n1+ B1 and V computes 1 =y2*n2+ B2. They run Y1, Y2 =MtA(A1, A2).
-    let (l1, l2) = (-p1.y * n1 + b1, p2.y * n2 + b2);
-    let (y1, y2) = mta(&l1, &l2);
+    // Compute cB
+    let c_bob = Paillier::add(&ek, b_mul_c_alice.clone(), enc_betatag.clone());
 
-    // P (and V) computes si =2; + λ;2 - xi for i = {1,2} respectively.
-    let (s1, s2) = (n1 + l1.square() - p1.x, n2 + l2.square() - p2.x);
+    // Bob sets additive share Beta = -BetaTag mod n
+    let beta = Scalar::<Secp256k1>::from(&(&BigInt::zero() - &beta_tag) % &ek.n);
 
-    // P outputs s1 and V outputs s2.
-    (s1, s2)
+    // Alice decrypts = dec(cB)
+    let dec_alice = Paillier::decrypt(&dk, &c_bob);
+
+    // Alice sets alpha = dec_alice mod n
+    let alpha = Scalar::<Secp256k1>::from(BigInt::from(dec_alice.clone()) % &ek.n);
+    (alpha, beta)
 }
 
+fn mta_2(
+    alice_secret: &Scalar<Secp256k1>,
+    r1: &BigInt,
+    r2: &BigInt,
+    bob_secret: &Scalar<Secp256k1>, 
+) -> (Scalar<Secp256k1>, Scalar<Secp256k1>) {
+
+    // Generate Paillier key pair
+    let (ek, dk) = Paillier::keypair().keys();
+
+    // Alice's secret shares
+    let a1 = &alice_secret.to_bigint();
+    // Bob's secret shares
+    let a2 = &bob_secret.to_bigint();
+    
+    // 1. Alice computes CA = EncryptA(a1)
+    let c_a = Paillier::encrypt(&ek, RawPlaintext::from(&a1.clone()));
+    // 2. Alice computes CR1 = EncryptA(r1)
+    let c_r1 = Paillier::encrypt(&ek, RawPlaintext::from(r1.clone()));
+
+    // 4. Bob selects β' <- ZN
+    let beta_prime = BigInt::sample_below(&ek.n);
+
+    // 5. Bob computes CB = (r2 * CA) + (a2 * CR1) + EncryptA(β')
+    let r2_mul_c_alice =  Paillier::mul(&ek, RawPlaintext::from(r2.clone()), c_a.clone());
+    let a2_mul_c_r1 = Paillier::mul(&ek, RawPlaintext::from(a2.clone()), c_r1.clone());
+    let enc_beta_prime = Paillier::encrypt(&ek, RawPlaintext::from(&beta_prime));
+
+
+    let c_bob = Paillier::add(&ek, r2_mul_c_alice.clone(), a2_mul_c_r1.clone());
+    let c_bob = Paillier::add(&ek, c_bob.clone(), enc_beta_prime.clone());
+
+    // 6. Bob sets additive share δ2 = -β′ mod q
+    let delta_2 = (BigInt::zero() - &beta_prime) % &ek.n;
+
+    // 8. Alice decrypts α' = dec(CB)
+    let dec_alice = Paillier::decrypt(&dk, &c_bob);
+
+    // 9. Alice sets δ 1 = α' mod q
+    let delta_1 = BigInt::from(dec_alice) % &ek.n;
+
+
+    // let left = &delta_1 + &delta_2;
+    // dbg!(&left);
+    // let right = (a1*r2) + (a2*r1);
+    // dbg!(&right);
+    // assert_eq!(left, right, "Verification failed: Left side ({}) is not equal to right side ({})", left, right);
+
+    // println!("MTA Test Satisfied");
+    (delta_1.into(), delta_2.into())
+}
+fn ectf_protocol(
+    p1: &Point<Secp256k1>,
+    p2: &Point<Secp256k1>,
+) -> (Scalar<Secp256k1>, Scalar<Secp256k1>) {
+    let x1 = p1.x_coord().unwrap();
+    let y1 = p1.y_coord().unwrap();
+    let x2 = p2.x_coord().unwrap();
+    let y2 = p2.y_coord().unwrap();
+
+    // Alice and Bob sample random values
+    let zp = Scalar::<Secp256k1>::group_order();   
+
+    let rho_1 = BigInt::sample_below(&zp);
+    let rho_2 = BigInt::sample_below(&zp);
+
+    // Alice and Bob run MTA
+    let (alpha_1, alpha_2) = mta_2(&Scalar::<Secp256k1>::from(-x1.clone()), &rho_1, &rho_2, &Scalar::<Secp256k1>::from(x2.clone()));
+
+    // Compute delta values
+    let delta_1 = -x1.clone() * rho_1.clone() + alpha_1.clone().to_bigint();
+    let delta_2 = x2.clone() * rho_2.clone() + alpha_2.clone().to_bigint();
+
+    let delta = delta_1 + delta_2;
+
+    // Compute eta values
+    let eta_1 = rho_1/delta.clone();
+    let eta_2 = rho_2/delta.clone();
+
+    // Run MTA for beta values
+    let (beta_1, beta_2) = mta_2(&Scalar::<Secp256k1>::from(-y1.clone()), &eta_1, &eta_2, &Scalar::<Secp256k1>::from(y2.clone()));
+
+    // Compute lambda values
+    let lambda_1 = -y1 * eta_1 + beta_1.clone().to_bigint();
+    let lambda_2 = y2 * eta_2 + beta_2.clone().to_bigint();
+
+    // Run MTA for gamma values
+    let (gamma_1, gamma_2) = mta(&Scalar::<Secp256k1>::from(lambda_1.clone()), &Scalar::<Secp256k1>::from(lambda_2.clone()));
+
+    // Compute final output s values
+    let s1 = BigInt::from(2) * gamma_1.to_bigint() + lambda_1.clone()*lambda_1.clone() - x1;
+    let s2 = BigInt::from(2) * gamma_2.to_bigint() + lambda_2.clone()*lambda_2.clone() - x2;
+
+    (s1.into(), s2.into())
+}
 fn main() {
     // Generate two points on the secp256k1 curve.
-    let p1 = Point::generator();
-    let p2 = Point::random();
+    let p1 = Point::<Secp256k1>::generator().to_point();
+    let p2 = Point::<Secp256k1>::generator().to_point();
 
     // Run the ECtF protocol.
     let (s1, s2) = ectf_protocol(&p1, &p2);
 
     // Check if s1 + s2 = x where (x, y) = p1 + p2.
     let p3 = p1 + p2;
-    assert_eq!(s1 + s2, p3.x);
+    assert_eq!(s1 + s2, Scalar::<Secp256k1>::from(p3.x_coord().unwrap()));
 
     println!("ECtF protocol completed successfully.");
 }
